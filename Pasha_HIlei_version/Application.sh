6280f7c6266bc2749fedcb905afdc9d378442b63
Pavlo Hilei <pavlikhiley@gmial.com>
2019-11-16 23:33:37 +0200

    Added my purple version.
    4 physical core, 8 logical, 1 root directory (/dev/sdb8), moved a little some elements, changed all colors, added new cool gif.

---
+++ Pasha_HIlei_version/Application.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+speedtest-cli --simple > ~/blood-and-milk/.speeds 2>&1 &
+
+gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/blood-and-milk/image/wall.png"
+
+sleep 7
+
+conky -c ~/blood-and-milk/connections_graph.conf &
+conky -c ~/blood-and-milk/disk/disk_graph.conf &
+conky -c ~/blood-and-milk/cpu_histograme.conf &
+
+sleep 3
+
+conky -c ~/blood-and-milk/disk/fs_disk.conf &
+conky -c ~/blood-and-milk/memory_graph.conf &
+
+sleep 3
+
+conky -c ~/blood-and-milk/temperature_rings.conf &
+
+sleep 3
+
+conky -c ~/blood-and-milk/gmail/gmail.conf &
+conky -c ~/blood-and-milk/cpu_rings.conf &
+conky -c ~/blood-and-milk/notes.conf &
+
+sleep 3
+
+conky -c ~/blood-and-milk/connections_list.conf &
+conky -c ~/blood-and-milk/processes_list.conf &
+
+sleep 10
+
+conky -c ~/blood-and-milk/image/globus_gif &
